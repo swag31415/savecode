ef548c86d61dbb7c10c431e4ce386060ac5e63b5
swag31415 <kidswagatam18@gmail.com>
2021-06-01 12:23:18 -0400

    this kinda works

---
+++ src/markdown.nim
@@ -0,0 +1,29 @@
+# Markdown helper functions
+
+from strutils import repeat, contains
+
+# Add a line to given string
+proc add_line*(str: var string; line: string) =
+  str.add(line & '\n')
+
+# Add some text
+proc add_text*(md: var string; text: string) =
+  md.add(text & '\n' & '\n')
+
+# Add a markdown title
+proc add_title*(md: var string; title: string; level: int) =
+  md.add_line(repeat('#', level) & ' ' & title)
+
+# Add a list
+proc add_list_item*(md: var string; text: string; level: int) =
+  md.add_line(repeat("  ", level) & "- " & text)
+
+# Add a code block
+proc add_code*(md: var string; code, lang: string) =
+  var fence = "```"
+  # Sanitation
+  while code.contains(fence):
+    fence.add('`')
+  md.add_line(fence & lang)
+  md.add_line(code)
+  md.add_line(fence)
--- src/savecode.nim
+++ src/savecode.nim
@@ -1,2 +1,86 @@
 ## savecode
 
+import os
+import osproc
+import markdown
+
+# Prompts the user and returns the response
+proc ask(msg: string): string =
+  stdout.write(msg)
+  return readLine(stdin)
+
+# Get the git repo
+var root = ask("Path to git repo: ")
+# Ensure its a valid directory
+while not dirExists(root):
+  root = ask("invalid response, try again: ")
+# Make it absolute
+root = absolutePath(root)
+
+# Move to the git repo
+setCurrentDir(root)
+
+# A string to store the markdown
+var md: string
+
+# Recursively encodes directory structure as a nested list
+proc rec_add_dir_struct(dir: string; level = 0) =
+  for kind, path in walkDir(dir):
+    # Ignore hidden stuff
+    if isHidden(path): continue
+    # Add items for folders and files
+    if kind == pcDir or kind == pcFile:
+      md.add_list_item(lastPathPart(path), level)
+      # Recursivly add folder contents
+      if kind == pcDir:
+        rec_add_dir_struct(path, level + 1)
+
+# Add Directory structure
+md.add_line("Directory structure")
+rec_add_dir_struct(root)
+
+# Add git graph
+const git_graph_cmd = "git log --pretty=fuller --abbrev-commit --all --graph --decorate"
+md.add_title("Git Graph", 1)
+md.add_code(execCmdEx(git_graph_cmd).output, "git")
+
+# Recursively reads and adds files to the markdown
+proc rec_add_files(dir: string; level: int) =
+  for kind, path in walkDir(dir):
+    # Ignore hidden stuff
+    if isHidden(path): continue
+    case kind:
+      of pcFile:
+        # Ignore big files
+        if getFileSize(path) > 500000:
+          echo "Skipping ", path, " because its too big"
+          continue
+        let (dir, name, dext) = splitFile(path)
+        # remove the dot
+        let ext = if dext != "": dext[1..^1] else: ""
+        let content = readFile(path)
+        # Add the section
+        case ext:
+          of "", "py", "nim", "nimble", "c", "h", "cpp", "java", "gradle", "md", "json", "js", "html", "ejs", "css", "bat", "ahk", "m":
+            md.add_title(name & dext, level)
+            md.add_code(content, ext)
+          of "classpath", "project", "fxml", "xml":
+            md.add_title(name & dext, level)
+            md.add_code(content, "xml")
+          of "txt", "gitignore", "gitattributes", "cfg", "log", "properties", "config":
+            md.add_title(name & dext, level)
+            md.add_code(content, "")
+          else: discard
+      # Recursively handle directories
+      of pcDir:
+        md.add_title(lastPathPart(path), level)
+        rec_add_files(path, level + 1)
+      # Don't worry about symblinks and stuff
+      else: discard
+
+# Add code files
+md.add_title("Code Files", 1)
+rec_add_files(root, 2)
+
+# Write to an actual markdown file
+writeFile(lastPathPart(root) & ".md", md)
